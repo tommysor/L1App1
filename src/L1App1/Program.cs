0f9e2f9b9d02c44e6d55e1c2fc95e5a3dd911582
Tommy Sørbråten <tommysor@gmail.com>
2023-07-24 21:58:40 +0000

    Add todos and dummy secured endpoint

--- src/L1App1/Program.cs
+++ src/L1App1/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
+
+//todo Add secrets from vault
+
+//todo Add authentication
+
+//todo Add authorization
 
 /*
  * Build
@@ -18,6 +28,29 @@ app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Hello World!")
    .WithName("Hello")
+   .WithOpenApi();
+
+app.MapGet("/Secured/{id}", (
+    [FromRoute]int id,
+    [FromServices]IHttpContextAccessor contextAccessor,
+    [FromServices]ILogger<Program> logger) => 
+{
+    try
+    {
+        var context = contextAccessor.HttpContext ?? throw new InvalidOperationException("HttpContext is null");
+        //todo if context.User.Identity NOT have permission to view object with id: {id} then return 403 Forbidden
+        var userName = context.User.Identity?.Name;
+        return TypedResults.Ok($"Hello {userName} here is id: {id}");
+    }
+    catch (Exception ex)
+    {
+        var guid = Guid.NewGuid();
+        logger.LogError(ex, "Unexpected error {@guid}", guid);
+        return (IResult)TypedResults.Problem(detail: $"Something went unexpectedly wrong. Reference: {guid}", statusCode: 500);
+    }
+})
+   .WithName("SecuredHello")
+//    .RequireAuthorization()
    .WithOpenApi();
 
 app.Run();
