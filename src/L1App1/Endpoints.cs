6b24d60894a9f8813fd5a33653fa83bb3fb63408
Tommy Sørbråten <tommysor@gmail.com>
2023-07-30 15:46:26 +0000

    Move endpoint implementation

---
+++ src/L1App1/Endpoints.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L1App1;
+
+public class Endpoints
+{
+    public static async Task<Results<Ok<string>, ProblemHttpResult>> SecuredEndpoint(
+        [FromRoute]int id,
+        [FromServices]IHttpContextAccessor contextAccessor,
+        [FromServices]ILogger<Endpoints> logger)
+{
+    try
+    {
+        await Task.CompletedTask;
+        var context = contextAccessor.HttpContext ?? throw new InvalidOperationException("HttpContext is null");
+        //todo if context.User.Identity NOT have permission to view object with id: {id} then return 403 Forbidden
+        var userName = context.User.Identity?.Name;
+        return TypedResults.Ok($"Hello {userName} here is id: {id}");
+    }
+    catch (Exception ex)
+    {
+        var guid = Guid.NewGuid();
+        logger.LogError(ex, "Unexpected error {@guid}", guid);
+        return TypedResults.Problem(detail: $"Something went unexpectedly wrong. Reference: {guid}", statusCode: 500);
+    }
+    }
+}
--- src/L1App1/Program.cs
+++ src/L1App1/Program.cs
@@ -1,6 +1,6 @@
+using L1App1;
 using L1App1.Authentications;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -75,31 +75,13 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!")
-   .WithName("Hello")
+   .WithTags("Hello")
    .WithOpenApi()
    .AllowAnonymous();
    ;
 
-app.MapGet("/Secured/{id}", (
-    [FromRoute]int id,
-    [FromServices]IHttpContextAccessor contextAccessor,
-    [FromServices]ILogger<Program> logger) => 
-{
-    try
-    {
-        var context = contextAccessor.HttpContext ?? throw new InvalidOperationException("HttpContext is null");
-        //todo if context.User.Identity NOT have permission to view object with id: {id} then return 403 Forbidden
-        var userName = context.User.Identity?.Name;
-        return TypedResults.Ok($"Hello {userName} here is id: {id}");
-    }
-    catch (Exception ex)
-    {
-        var guid = Guid.NewGuid();
-        logger.LogError(ex, "Unexpected error {@guid}", guid);
-        return (IResult)TypedResults.Problem(detail: $"Something went unexpectedly wrong. Reference: {guid}", statusCode: 500);
-    }
-})
-   .WithName("SecuredHello")
+app.MapGet("Secured/{id}", Endpoints.SecuredEndpoint)
+   .WithTags("Secured")
    .RequireAuthorization("User")
    .WithOpenApi();
 
